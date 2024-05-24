b931dc431a91051085ddafb62ea2d417b8bc4aab
tugaycan <tcduymaz94@gmail.com>
2024-05-24 17:58:32 +0300

    new offer page done

--- src/pages/pre-built/offers/newOffersPage.js
+++ src/pages/pre-built/offers/newOffersPage.js
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from "react";
 import Content from "../../../layout/content/Content";
-import { Label, Input, Row, Col, UncontrolledDropdown, DropdownToggle, DropdownMenu } from "reactstrap";
+import { Label, Input, Row, Col, UncontrolledDropdown, DropdownToggle, DropdownMenu, ButtonGroup, ButtonToolbar } from "reactstrap";
 import DatePicker from "react-datepicker";
 import {
     Block,
@@ -23,14 +23,28 @@ const NewOffersPage = () => {
 
     const [startDate, setStartDate] = useState(null);
     const [offer, setOffer] = useState([])
+    const [selectedTax, setSelectedTax] = useState(null);
+    const [discount, setDiscount] = useState({
+        label: "0%",
+        value: "0"
+    });
+
+
     const [newOfferData, setNewOfferData] = useState({
+        id: "",
         productName: "",
         comment: "",
         piece: "",
         price: "",
-        tax: "1.2",
+        tax: {},
         amount: "",
     });
+    const handleTaxChange = (selectedOption) => {
+        setSelectedTax(selectedOption);
+    };
+    const handleDiscountChange = (selectedOption) => {
+        setDiscount(selectedOption);
+    };
     const handleInputChange = (e) => {
         const { name, value } = e.target;
 
@@ -40,29 +54,78 @@ const NewOffersPage = () => {
         }));
     };
     const handleAddOffer = () => {
-        // Yeni teklif nesnesini oluştur
+
         const newOfferItem = {
+            id: offer.length + 1,
             productName: newOfferData.productName,
             comment: newOfferData.comment,
             piece: newOfferData.piece,
             price: newOfferData.price,
-            tax: newOfferData.tax,
+            tax: selectedTax,
             amount: newOfferData.amount
         };
 
-        // Eski tekliflerin üzerine yeni teklifi ekle
-        setOffer(prevOffer => [...prevOffer, newOfferItem]);
 
-        // Yeni teklif verilerini sıfırla
+        setOffer(prevOffer => [...prevOffer, newOfferItem]);
+        setSelectedTax(null)
+
         setNewOfferData({
+            id: "",
             productName: "",
             comment: "",
             piece: "",
             price: "",
-            tax: "1.2",
+            tax: {},
             amount: ""
         });
     };
+    const calculateSubtotal = () => {
+        let subtotal = 0;
+        offer.forEach(item => {
+
+            const totalItemPrice = parseFloat(item.piece) * parseFloat(item.price) * parseFloat(item.tax.value);
+            subtotal += totalItemPrice;
+        });
+        return subtotal.toFixed(2);
+    };
+    const calculateDiscountSubtotal = () => {
+        let discountSubtotal = 0;
+        discountSubtotal = calculateSubtotal() * discount.value
+        return discountSubtotal.toFixed(2);
+    };
+    const handleDeleteOffer = (id) => {
+        setOffer(offer.filter(item => item.id !== id))
+    }
+    console.log(offer)
+
+    const taxData = [
+        {
+            label: "20%",
+            value: "1.2"
+        },
+        {
+            label: "15%",
+            value: "1.15"
+        }
+    ];
+    const discountData = [
+        {
+            label: "0%",
+            value: "0"
+        },
+        {
+            label: "5%",
+            value: "0.05"
+        },
+        {
+            label: "10%",
+            value: "0.1"
+        },
+        {
+            label: "15%",
+            value: "0.15"
+        }
+    ];
 
     return (
         <Content>
@@ -71,13 +134,8 @@ const NewOffersPage = () => {
                     <BlockHeadContent>
                         <BlockTitle>Teklif Oluştur</BlockTitle>
                         <div className="nk-block-des text-soft"><p>Yeni Teklif Oluşturun</p></div>
-
                     </BlockHeadContent>
-
-
-
                 </BlockBetween>
-
             </BlockHead>
             <Block size="lg">
 
@@ -301,7 +359,8 @@ const NewOffersPage = () => {
                                             <div className="form-group">
 
                                                 <div className="form-control-wrap">
-                                                    <RSelect placeholder="Vergi" />
+                                                    <RSelect value={selectedTax} options={taxData}
+                                                        onChange={(selectedOption) => handleTaxChange(selectedOption)} placeholder="Vergi" />
                                                 </div>
                                             </div>
                                         </Col>
@@ -313,7 +372,9 @@ const NewOffersPage = () => {
                                         <Col xl="1" lg="6" className="pb-xl-0 pb-3">
                                             <strong className="text-dark"></strong>
                                             <hr className="d-xl-block d-none"></hr>
-                                            <button onClick={handleAddOffer} className="btn btn-primary btn-icon"><Icon name="check"></Icon></button>
+
+                                            <button onClick={handleAddOffer} className="btn btn-primary btn-icon d-xl-flex d-none"><Icon name="check"></Icon></button>
+                                            <button onClick={handleAddOffer} className="btn btn-primary d-xl-none d-block">Yeni Fatura Ekle</button>
                                         </Col>
                                     </Row>
 
@@ -334,7 +395,7 @@ const NewOffersPage = () => {
                                                 <div className="form-group">
 
                                                     <div className="form-control-wrap">
-                                                        <textarea name="productName" value={item.productName} onChange={handleInputChange} style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
+                                                        <textarea readOnly value={item.productName} name="productName" style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
                                                             placeholder="Ürün Adı">
                                                         </textarea>
                                                     </div>
@@ -346,7 +407,7 @@ const NewOffersPage = () => {
                                                 <div className="form-group ">
 
                                                     <div className="form-control-wrap">
-                                                        <textarea name="comment" value={item.comment} onChange={handleInputChange} style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
+                                                        <textarea readOnly value={item.comment} name="comment" style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
                                                             placeholder="Açıklama">
                                                         </textarea>
                                                     </div>
@@ -358,7 +419,7 @@ const NewOffersPage = () => {
                                                 <div className="form-group">
 
                                                     <div className="form-control-wrap">
-                                                        <input name="piece" value={item.piece} onChange={handleInputChange} id="default-0" placeholder="Adet" type="text" className="form-control" />
+                                                        <input readOnly value={item.piece} name="piece" id="default-0" placeholder="Adet" type="text" className="form-control" />
                                                     </div>
                                                 </div>
                                             </Col>
@@ -368,7 +429,7 @@ const NewOffersPage = () => {
                                                 <div className="form-group">
 
                                                     <div className="form-control-wrap">
-                                                        <input name="price" value={item.price} onChange={handleInputChange} id="default-0" placeholder="Tutar" type="text" className="form-control" />
+                                                        <input readOnly value={item.price} name="price" id="default-0" placeholder="Tutar" type="text" className="form-control" />
                                                     </div>
                                                 </div>
                                             </Col>
@@ -378,20 +439,21 @@ const NewOffersPage = () => {
                                                 <div className="form-group">
 
                                                     <div className="form-control-wrap">
-                                                        <RSelect placeholder="Vergi" />
+                                                        <input className="form-control" readOnly value={item.tax.label} placeholder="Vergi" />
                                                     </div>
                                                 </div>
                                             </Col>
                                             <Col xl="1" lg="6" className="pb-xl-0 pb-3">
                                                 <strong className="text-dark">Tutar</strong>
                                                 <hr className="d-xl-block d-none"></hr>
-                                                <p>{(parseFloat(item.piece) * parseFloat(item.price) * parseFloat(item.tax)).toFixed(2)}₺</p>
+                                                <p>{(parseFloat(item.piece) * parseFloat(item.price) * (item.tax.value)).toFixed(2)}₺</p>
 
                                             </Col>
                                             <Col xl="1" lg="6" className="pb-xl-0 pb-3">
                                                 <strong className="text-dark"></strong>
                                                 <hr className="d-xl-block d-none"></hr>
-                                                <button className="btn btn-primary btn-icon"><Icon name="trash-fill"></Icon></button>
+                                                <button onClick={() => handleDeleteOffer(item.id)} className="btn btn-primary btn-icon d-xl-flex d-none"><Icon name="trash-fill"></Icon></button>
+                                                <Button onClick={() => handleDeleteOffer(item.id)} className="d-xl-none d-block" color="danger">Faturayı Sil</Button>
                                             </Col>
                                         </Row>
 
@@ -399,7 +461,62 @@ const NewOffersPage = () => {
                                 </Row>
                             </div>
                         ))}
+                        {offer.length > 0 && (
+                            <Row>
 
+                                <Col xl="6" className="d-xl-block d-none">
+                                </Col>
+                                <Col xl="6" className="mt-0">
+                                    <hr className=""></hr>
+                                    <div className="d-flex justify-content-xl-end justify-content-between">
+                                        <strong className="px-2">
+                                            Ara Toplam</strong>
+                                        <strong className="px-2">{calculateSubtotal()}₺</strong>
+                                    </div>
+                                </Col>
+                                <Col xl="6" className="d-xl-block d-none">
+                                </Col>
+                                <Col xl="6" className="mt-0">
+                                    <hr className=""></hr>
+                                    <div className="d-flex justify-content-xl-end justify-content-between align-items-center">
+
+                                        <div className="form-group mb-0" style={{ width: "105px" }}>
+
+                                            <div className="form-control-wrap">
+                                                <RSelect value={discount} options={discountData}
+                                                    onChange={(selectedOption) => handleDiscountChange(selectedOption)} placeholder="İndirim" />
+                                            </div>
+                                        </div>
+                                        <strong className="px-2">-{calculateDiscountSubtotal()}₺</strong>
+                                    </div>
+                                </Col>
+                                <Col xl="6" className="d-xl-block d-none">
+                                </Col>
+                                <Col xl="6" className="mt-0">
+                                    <hr className=""></hr>
+                                    <div className="d-flex justify-content-xl-end justify-content-between" >
+                                        <strong className="px-2">
+                                            Toplam</strong>
+                                        <strong className="px-2">{(calculateSubtotal() - calculateDiscountSubtotal()).toFixed(2)}₺</strong>
+                                    </div>
+                                </Col>
+                                <Col sm="12" className="mt-0">
+
+                                    <hr className=""></hr>
+                                    <div className="d-flex justify-content-xl-end justify-content-between">
+                                        <ButtonToolbar className="g-2">
+                                            <ButtonGroup>
+                                                <Button outline color="secondary">Vazgeç</Button>
+                                            </ButtonGroup>
+                                            <ButtonGroup>
+                                                <Button color="primary">Kaydet</Button>
+                                            </ButtonGroup>
+                                        </ButtonToolbar>
+                                    </div>
+                                </Col>
+
+                            </Row>
+                        )}
 
                     </Row>
 
