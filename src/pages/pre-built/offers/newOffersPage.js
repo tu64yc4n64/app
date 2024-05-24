6eb9a0d6c94cc05e8f54008d9a136cfa5d819a09
tugaycan <tcduymaz94@gmail.com>
2024-05-24 12:15:21 +0300

    added new offer function

--- src/pages/pre-built/offers/newOffersPage.js
+++ src/pages/pre-built/offers/newOffersPage.js
@@ -22,17 +22,48 @@ import "./style.css"
 const NewOffersPage = () => {
 
     const [startDate, setStartDate] = useState(null);
-    const [newOfferData, setNewOfferDatatDate] = useState([{
+    const [offer, setOffer] = useState([])
+    const [newOfferData, setNewOfferData] = useState({
         productName: "",
         comment: "",
         piece: "",
         price: "",
-        tax: "0.2",
+        tax: "1.2",
         amount: "",
-    }]);
+    });
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
 
+        setNewOfferData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+    const handleAddOffer = () => {
+        // Yeni teklif nesnesini oluştur
+        const newOfferItem = {
+            productName: newOfferData.productName,
+            comment: newOfferData.comment,
+            piece: newOfferData.piece,
+            price: newOfferData.price,
+            tax: newOfferData.tax,
+            amount: newOfferData.amount
+        };
 
-    console.log(newOfferData)
+        // Eski tekliflerin üzerine yeni teklifi ekle
+        setOffer(prevOffer => [...prevOffer, newOfferItem]);
+
+        // Yeni teklif verilerini sıfırla
+        setNewOfferData({
+            productName: "",
+            comment: "",
+            piece: "",
+            price: "",
+            tax: "1.2",
+            amount: ""
+        });
+    };
+
     return (
         <Content>
             <BlockHead size="sm">
@@ -213,79 +244,163 @@ const NewOffersPage = () => {
 
 
                         </Col>
-                        <div className="bg-lighter p-3 new-offer-create">
-                            <Col md="12">
+                        <div className="bg-lighter py-3 new-offer-create">
+                            <Row>
+
+                                <Col md="12">
 
 
+                                    <Row>
+                                        <Col xl="2" lg="6" className="pb-xl-0 pb-3">
+                                            <strong className="text-dark">Ürün Adı</strong>
+                                            <hr className="d-xl-block d-none"></hr>
+                                            <div className="form-group">
+
+                                                <div className="form-control-wrap">
+                                                    <textarea name="productName" value={newOfferData.productName} onChange={handleInputChange} style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
+                                                        placeholder="Ürün Adı">
+                                                    </textarea>
+                                                </div>
+                                            </div>
+                                        </Col>
+                                        <Col xl="3" lg="6" className="pb-xl-0 pb-3">
+                                            <strong className="text-dark">Açıklama</strong>
+                                            <hr className="d-xl-block d-none"></hr>
+                                            <div className="form-group ">
+
+                                                <div className="form-control-wrap">
+                                                    <textarea name="comment" value={newOfferData.comment} onChange={handleInputChange} style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
+                                                        placeholder="Açıklama">
+                                                    </textarea>
+                                                </div>
+                                            </div>
+                                        </Col>
+                                        <Col xl="1" lg="6" className="pb-xl-0 pb-3">
+                                            <strong className="text-dark">Adet</strong>
+                                            <hr className="d-xl-block d-none"></hr>
+                                            <div className="form-group">
+
+                                                <div className="form-control-wrap">
+                                                    <input name="piece" value={newOfferData.piece} onChange={handleInputChange} id="default-0" placeholder="Adet" type="text" className="form-control" />
+                                                </div>
+                                            </div>
+                                        </Col>
+                                        <Col xl="2" lg="6" className="pb-xl-0 pb-3">
+                                            <strong className="text-dark">Birim Fiyat (Vergi Dahil)</strong>
+                                            <hr className="d-xl-block d-none"></hr>
+                                            <div className="form-group">
+
+                                                <div className="form-control-wrap">
+                                                    <input name="price" value={newOfferData.price} onChange={handleInputChange} id="default-0" placeholder="Tutar" type="text" className="form-control" />
+                                                </div>
+                                            </div>
+                                        </Col>
+                                        <Col xl="2" lg="6" className="pb-xl-0 pb-3">
+                                            <strong className="text-dark">Vergi</strong>
+                                            <hr className="d-xl-block d-none"></hr>
+                                            <div className="form-group">
+
+                                                <div className="form-control-wrap">
+                                                    <RSelect placeholder="Vergi" />
+                                                </div>
+                                            </div>
+                                        </Col>
+                                        <Col xl="1" lg="6" className="pb-xl-0 pb-3">
+                                            <strong className="text-dark">Tutar</strong>
+                                            <hr className="d-xl-block d-none"></hr>
+
+                                        </Col>
+                                        <Col xl="1" lg="6" className="pb-xl-0 pb-3">
+                                            <strong className="text-dark"></strong>
+                                            <hr className="d-xl-block d-none"></hr>
+                                            <button onClick={handleAddOffer} className="btn btn-primary btn-icon"><Icon name="check"></Icon></button>
+                                        </Col>
+                                    </Row>
+
+                                </Col>
+                            </Row>
+                        </div>
+                        {offer.map((item, i) => (
+                            <div key={i} className=" py-3 new-offer-create">
                                 <Row>
-                                    <Col xl="2" lg="6" className="pb-xl-0 pb-3">
-                                        <strong className="text-dark">Ürün Adı</strong>
-                                        <hr className="d-xl-block d-none"></hr>
-                                        <div className="form-group">
 
-                                            <div className="form-control-wrap">
-                                                <textarea onChange={(e) => setNewOfferDatatDate({ productName: e.target.value })} style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
-                                                    placeholder="Ürün Adı">
-                                                </textarea>
-                                            </div>
-                                        </div>
-                                    </Col>
-                                    <Col xl="3" lg="6" className="pb-xl-0 pb-3">
-                                        <strong className="text-dark">Açıklama</strong>
-                                        <hr className="d-xl-block d-none"></hr>
-                                        <div className="form-group ">
+                                    <Col md="12">
 
-                                            <div className="form-control-wrap">
-                                                <textarea onChange={(e) => setNewOfferDatatDate({ comment: e.target.value })} style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
-                                                    placeholder="Açıklama">
-                                                </textarea>
-                                            </div>
-                                        </div>
-                                    </Col>
-                                    <Col xl="1" lg="6" className="pb-xl-0 pb-3">
-                                        <strong className="text-dark">Adet</strong>
-                                        <hr className="d-xl-block d-none"></hr>
-                                        <div className="form-group">
 
-                                            <div className="form-control-wrap">
-                                                <input onChange={(e) => setNewOfferDatatDate({ piece: e.target.value })} id="default-0" placeholder="Adet" type="text" className="form-control" />
-                                            </div>
-                                        </div>
-                                    </Col>
-                                    <Col xl="2" lg="6" className="pb-xl-0 pb-3">
-                                        <strong className="text-dark">Birim Fiyat (Vergi Dahil)</strong>
-                                        <hr className="d-xl-block d-none"></hr>
-                                        <div className="form-group">
+                                        <Row>
+                                            <Col xl="2" lg="6" className="pb-xl-0 pb-3">
+                                                <strong className="text-dark">Ürün Adı</strong>
+                                                <hr className="d-xl-block d-none"></hr>
+                                                <div className="form-group">
 
-                                            <div className="form-control-wrap">
-                                                <input onChange={(e) => setNewOfferDatatDate({ price: e.target.value })} id="default-0" placeholder="Tutar" type="text" className="form-control" />
-                                            </div>
-                                        </div>
-                                    </Col>
-                                    <Col xl="2" lg="6" className="pb-xl-0 pb-3">
-                                        <strong className="text-dark">Vergi</strong>
-                                        <hr className="d-xl-block d-none"></hr>
-                                        <div className="form-group">
+                                                    <div className="form-control-wrap">
+                                                        <textarea name="productName" value={item.productName} onChange={handleInputChange} style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
+                                                            placeholder="Ürün Adı">
+                                                        </textarea>
+                                                    </div>
+                                                </div>
+                                            </Col>
+                                            <Col xl="3" lg="6" className="pb-xl-0 pb-3">
+                                                <strong className="text-dark">Açıklama</strong>
+                                                <hr className="d-xl-block d-none"></hr>
+                                                <div className="form-group ">
 
-                                            <div className="form-control-wrap">
-                                                <RSelect placeholder="Vergi" />
-                                            </div>
-                                        </div>
-                                    </Col>
-                                    <Col xl="1" lg="6" className="pb-xl-0 pb-3">
-                                        <strong className="text-dark">Tutar</strong>
-                                        <hr className="d-xl-block d-none"></hr>
+                                                    <div className="form-control-wrap">
+                                                        <textarea name="comment" value={item.comment} onChange={handleInputChange} style={{ minHeight: "80px" }} id="default-textarea" className="no-resize form-control"
+                                                            placeholder="Açıklama">
+                                                        </textarea>
+                                                    </div>
+                                                </div>
+                                            </Col>
+                                            <Col xl="1" lg="6" className="pb-xl-0 pb-3">
+                                                <strong className="text-dark">Adet</strong>
+                                                <hr className="d-xl-block d-none"></hr>
+                                                <div className="form-group">
 
-                                    </Col>
-                                    <Col xl="1" lg="6" className="pb-xl-0 pb-3">
-                                        <strong className="text-dark"></strong>
-                                        <hr className="d-xl-block d-none"></hr>
-                                        <button className="btn btn-primary btn-icon"><Icon name="check"></Icon></button>
+                                                    <div className="form-control-wrap">
+                                                        <input name="piece" value={item.piece} onChange={handleInputChange} id="default-0" placeholder="Adet" type="text" className="form-control" />
+                                                    </div>
+                                                </div>
+                                            </Col>
+                                            <Col xl="2" lg="6" className="pb-xl-0 pb-3">
+                                                <strong className="text-dark">Birim Fiyat (Vergi Dahil)</strong>
+                                                <hr className="d-xl-block d-none"></hr>
+                                                <div className="form-group">
+
+                                                    <div className="form-control-wrap">
+                                                        <input name="price" value={item.price} onChange={handleInputChange} id="default-0" placeholder="Tutar" type="text" className="form-control" />
+                                                    </div>
+                                                </div>
+                                            </Col>
+                                            <Col xl="2" lg="6" className="pb-xl-0 pb-3">
+                                                <strong className="text-dark">Vergi</strong>
+                                                <hr className="d-xl-block d-none"></hr>
+                                                <div className="form-group">
+
+                                                    <div className="form-control-wrap">
+                                                        <RSelect placeholder="Vergi" />
+                                                    </div>
+                                                </div>
+                                            </Col>
+                                            <Col xl="1" lg="6" className="pb-xl-0 pb-3">
+                                                <strong className="text-dark">Tutar</strong>
+                                                <hr className="d-xl-block d-none"></hr>
+                                                <p>{(parseFloat(item.piece) * parseFloat(item.price) * parseFloat(item.tax)).toFixed(2)}₺</p>
+
+                                            </Col>
+                                            <Col xl="1" lg="6" className="pb-xl-0 pb-3">
+                                                <strong className="text-dark"></strong>
+                                                <hr className="d-xl-block d-none"></hr>
+                                                <button className="btn btn-primary btn-icon"><Icon name="trash-fill"></Icon></button>
+                                            </Col>
+                                        </Row>
+
                                     </Col>
                                 </Row>
+                            </div>
+                        ))}
 
-                            </Col>
-                        </div>
+
                     </Row>
 
 
