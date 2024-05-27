19402f5a47d4f869cbfc7df43bc25d5d68c378f8
tugaycan <tcduymaz94@gmail.com>
2024-05-27 12:51:57 +0300

    changed port

--- src/api/server.js
+++ src/api/server.js
@@ -10,7 +10,7 @@ server.use(middlewares);
 server.use(rewriter);
 server.use(router);
 
-const port = 3000;
+const port = 3001;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
