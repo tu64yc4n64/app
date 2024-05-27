c882b41d2a0ff623e6d9cc6fa2b03033848fc613
tugaycan <tcduymaz94@gmail.com>
2024-05-27 12:44:48 +0300

    delete json-server

---
+++ src/api/server.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const rewriter = jsonServer.rewriter({
+  "/api/*": "/$1"
+});
+
+server.use(middlewares);
+server.use(rewriter);
+server.use(router);
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
